f5f6e6b28bf81743c8a9e1a3e4ad80d15bdded2d
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-01 22:24:12 -0800

    I like readr::read_lines()

--- day02.R
+++ day02.R
@@ -1,9 +1,11 @@
 library(purrr)
 library(magrittr)
+library(readr)
 library(testthat)
 
 prep <- function(txt) {
-  scan(text = txt, what = "character", sep = "\n") %>%
+  txt %>%
+    read_lines() %>%
     strsplit("\\s") %>%
     map(as.integer)
 }
@@ -21,11 +23,11 @@ checksum <- function(xl, f) sum(map_dbl(xl, f))
 x <- prep("5 1 9 5\n7 5 3\n2 4 6 8")
 expect_equal(checksum(x, span), 18)
 
-x <- prep(readLines("day02_input.txt"))
+x <- prep("day02_input.txt")
 checksum(x, span)
 
 x <- prep("5 9 2 8\n9 4 7 3\n3 8 6 5")
 expect_equal(checksum(x, dvr), 9)
 
-x <- prep(readLines("day02_input.txt"))
+x <- prep("day02_input.txt")
 checksum(x, dvr)
